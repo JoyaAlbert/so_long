4cf86f9e1f2e6af7b0dbe4c05ff890314728b15f
JoyaAlbert <albertojoyapincu2@gmail.com>
2024-02-12 11:10:11 +0100

    a

--- maps.c
+++ maps.c
@@ -18,28 +18,22 @@ int	nameformat(char *map)
 	return 1;
 }
 
-char **fillmatrix(int dato, char *mapa, char **maps)
+char **fillmatrix(char **map, int fd)
 {
-	int	j;
-	int	i;
+	char	*aux;
+	int		i;
 
-	j = 0;
-	i = 0;
-	while (j < dato)
+	aux = get_next_line(fd);
+	while (aux != NULL)
 	{
-		while (mapa[i] != '\n')
-		{
-			maps[j][i] = mapa[i];
-			i++;
-		}
-		maps[j][i] = '\0';
-		j++;
+		map[i] = aux;
 		i++;
+		aux = get_next_line(fd);
 	}
-	return (maps);
+	return(map);
 }
 
-char **matrix(int width, char *mapa, char *aux, int fd) 
+char **matrix(int width, char *aux, int fd) 
 {
 	char **maps;
 	int i;
@@ -79,8 +73,11 @@ int	mapchecker(char	*map_name)
 		return (-1);
 	int	fd = open(map_name, O_RDONLY);
 	aux = get_next_line(fd);
-	map = matrix(ft_strlen(aux), map_name, aux, fd);
+	map = matrix(ft_strlen(aux), aux, fd);
 	close(fd); //volver a abrir y rellenar la matriz
+	fd = open(map_name, O_RDONLY);
+	map = fillmatrix(map,fd);
+	close(fd);
 	return 1;
 }
 int main()

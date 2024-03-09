f2df3085aa5dfabc70e72cfdf2f2ff3c8e567911
JoyaAlbert <91436479+JoyaAlbert@users.noreply.github.com>
2024-03-09 12:35:52 +0100

    Delete aaa directory

--- aaa/so_long.h
+++
@@ -1,9 +0,0 @@
-#include "../gnl/get_next_line.h"
-#include "../printf/ft_printf.h"
-#include <stdio.h>
-
-char    **mapchecker(char *map);
-int	    matrixtam(int fd);
-char    **matrix(int width,int length);
-char    **fillmatrix(char **map, int fd, char *map_name);
-int     nameformat(char *map);

922482cfc13998a88a599d44cca4fa567887bcd4
JoyaAlbert <albertojoyapincu2@gmail.com>
2024-04-12 22:41:55 +0200

    a

--- imput_aux.c
+++ imput_aux.c
@@ -2,6 +2,24 @@
 #include "mlx/mlx.h"
 
 
+int inputs(int input, t_list *a)
+{
+    p_pos(a);
+	if (input == 48)
+    {
+		mlx_destroy_window(a->mlx, a->win);
+        exit (0);
+    }
+    if(input == (int)'W' || input == (int)'w')
+        mv_up(a);
+    if(input == (int)'S' || input == (int)'s')
+        mv_down(a);
+    if(input == (int)'A' || input == (int)'a')
+        mv_left(a);   
+    if(input == (int)'D' || input == (int)'d')
+        mv_right(a);
+    return 1;
+}
 void	p_pos(t_list *a)
 {
 	int i;
